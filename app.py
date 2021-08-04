880718a3b1d41a867312a7723a259e9f7661de6f
Alessandro Cere <ale.cere@gmail.com>
2021-08-04 11:20:18 +0800

    Added object key prefix for assets

--- app.py
+++ app.py
@@ -13,24 +13,24 @@ logging.basicConfig(level="INFO")
 # Create App and stacks
 app = core.App()
 
-artifact_bucket = app.node.try_get_context("ArtifactBucket")
+# Attempts to retrive custom bucket name and object key prefix for deployable assets.
+artifact_bucket = app.node.try_get_context("drift:ArtifactBucket")
+artifact_bucket_prefix = app.node.try_get_context("drift:ArtifactBucketPrefix")
 
 # Create the pipeline stack
-synth = core.DefaultStackSynthesizer()
-if artifact_bucket is not None:
-    synth = core.DefaultStackSynthesizer(
-        file_assets_bucket_name=artifact_bucket,
-        generate_bootstrap_version_rule=False,
-    )
+synth = core.DefaultStackSynthesizer(
+    file_assets_bucket_name=artifact_bucket,
+    generate_bootstrap_version_rule=False,
+    bucket_prefix=artifact_bucket_prefix,
+)
 PipelineStack(app, "drift-pipeline", synthesizer=synth)
 
 # Create the SC stack
-synth = core.DefaultStackSynthesizer()
-if artifact_bucket is not None:
-    synth = core.DefaultStackSynthesizer(
-        file_assets_bucket_name=artifact_bucket,
-        generate_bootstrap_version_rule=False,
-    )
+synth = core.DefaultStackSynthesizer(
+    file_assets_bucket_name=artifact_bucket,
+    generate_bootstrap_version_rule=False,
+    bucket_prefix=artifact_bucket_prefix,
+)
 
 ServiceCatalogStack(app, "drift-service-catalog", synthesizer=synth)
 
