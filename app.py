967ab1847eab66c281cabb086870c8ed10cb9199
Alessandro Cere <13648166+acere@users.noreply.github.com>
2021-08-05 11:12:25 +0800

    WIP: Added option to select bucket name for deployable assets (#6)
    * Added option to select bucket name for deployable assets
    
    * Added object key prefix for assets

--- app.py
+++ app.py
@@ -13,10 +13,25 @@ logging.basicConfig(level="INFO")
 # Create App and stacks
 app = core.App()
 
+# Attempts to retrive custom bucket name and object key prefix for deployable assets.
+artifact_bucket = app.node.try_get_context("drift:ArtifactBucket")
+artifact_bucket_prefix = app.node.try_get_context("drift:ArtifactBucketPrefix")
+
 # Create the pipeline stack
-PipelineStack(app, "drift-pipeline")
+synth = core.DefaultStackSynthesizer(
+    file_assets_bucket_name=artifact_bucket,
+    generate_bootstrap_version_rule=False,
+    bucket_prefix=artifact_bucket_prefix,
+)
+PipelineStack(app, "drift-pipeline", synthesizer=synth)
 
 # Create the SC stack
-ServiceCatalogStack(app, "drift-service-catalog")
+synth = core.DefaultStackSynthesizer(
+    file_assets_bucket_name=artifact_bucket,
+    generate_bootstrap_version_rule=False,
+    bucket_prefix=artifact_bucket_prefix,
+)
+
+ServiceCatalogStack(app, "drift-service-catalog", synthesizer=synth)
 
 app.synth()
